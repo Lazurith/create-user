7ccdb24a0720f3d974221ded3fd44734997f0375
LazizKhamrakulov <lazizkhamrakulov@gmail.com>
2025-02-15 19:30:52 +0500

    Add files via upload

---
+++ new-react-project/src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+// style
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className="footer-container container">
+        <p> by <a href="https://github.com/Lazurith">me</a></p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
